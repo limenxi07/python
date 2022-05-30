a60ae27bfd5333a2f2de5984fa62c88fbb6de0f6
Lim En Xi <limenxi07@gmail.com>
2022-05-30 19:36:02 +0800

    init

---
+++ 7-automated-projects/instagram-follower-bot.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import os, time
+USERNAME = os.environ('USERNAME')
+PASSWORD = os.environ('PASSWORD')
+CHROMEDRIVER = '/Users/limenxi/Documents/vscode/chromedriver'
