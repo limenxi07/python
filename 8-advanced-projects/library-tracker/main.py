1361275d067e07f0552447bbd00ef2509b31b3d1
Lim En Xi <limenxi07@gmail.com>
2022-06-07 15:45:33 +0800

    db functionality with sql

--- 8-advanced-projects/library-tracker/main.py
+++ 8-advanced-projects/library-tracker/main.py
@@ -1,11 +1,27 @@
 from flask import Flask, redirect, render_template, request, url_for
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-all_books = []
+
+# create database
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# create table
+class Book(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(250), unique=True, nullable=False)
+  author = db.Column(db.String(250), nullable=False)
+  rating = db.Column(db.Float, nullable=False)
+
+  def __repr__(self):
+    return f'<Book {self.title}>'
+db.create_all()
 
 
 @app.route('/')
 def home():
+  all_books = db.session.query(Book).all()
   return render_template('index.html', books=all_books)
 
 
@@ -17,7 +33,9 @@ def add():
       'author': request.form['author'],
       'rating': request.form['rating']
     }
-    all_books.append(new)
+    new = Book(title=request.form['title'], author=request.form['author'], rating=request.form['rating'])
+    db.session.add(new)
+    db.session.commit()
     return redirect(url_for('home'))
   else:
     return render_template('add.html')
