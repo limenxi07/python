bc3e05bfa02c7844c5a677a9db1e2670e980b058
Lim En Xi <limenxi07@gmail.com>
2022-06-07 08:20:23 +0800

    basic website functionality

--- 8-advanced-projects/library-tracker/main.py
+++ 8-advanced-projects/library-tracker/main.py
@@ -1,20 +1,28 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
-
 all_books = []
 
 
 @app.route('/')
 def home():
-    pass
+  return render_template('index.html', books=all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    pass
+  if request.method == 'POST':
+    new = {
+      'title': request.form['title'],
+      'author': request.form['author'],
+      'rating': request.form['rating']
+    }
+    all_books.append(new)
+    return redirect(url_for('home'))
+  else:
+    return render_template('add.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
 
