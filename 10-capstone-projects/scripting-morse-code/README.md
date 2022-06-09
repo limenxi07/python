635f101331a409e9fb16509dc793be695f8b0baa
Lim En Xi <limenxi07@gmail.com>
2022-06-09 14:08:11 +0800

    init

---
+++ 10-capstone-projects/scripting-morse-code/README.md
@@ -0,0 +1,8 @@
+## Description
+Text-based (command line) program that takes any string input and converts it into Morse Code. 
+
+## Usage
+- Takes inputs using '.' and '-'. 
+- While the system operates in English, any input containing latin alphabet will work. 
+- Separates letters with spaces.
+- Separates words with '/'.
---
+++ 10-capstone-projects/scripting-morse-code/main.py
@@ -0,0 +1,41 @@
+TEXT_TO_MORSE = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.'}
+MORSE_TO_TEXT = {value:key for key, value in TEXT_TO_MORSE.items()}
+cont = True
+
+def operate():
+  dirn = True
+  while dirn:
+    dirn = input("Enter 'ENG' to convert English into Morse Code, and enter 'MOR' to convert Morse Code into English. ")
+    if dirn == 'ENG':
+      msg = input('Enter your message:\n').upper()
+      out = []
+      for i in msg:
+        if i in TEXT_TO_MORSE:
+          out.append(f'{TEXT_TO_MORSE[i]} ')
+        elif i == ' ':
+          out.append(' / ')
+        else:
+          out.append(i)
+      print('Your translated message:\n', ''.join(out))
+      break
+    elif dirn == 'MOR':
+      msg = input('Enter your message:\n').split(' ')
+      print(msg)
+      out = []
+      for i in msg:
+        if i in MORSE_TO_TEXT:
+          out.append(MORSE_TO_TEXT[i])
+        elif i == '/':
+          out.append(' ')
+        else:
+          out.append(i)
+      print('Your translated message:\n', ''.join(out))
+      break
+    else:
+      print('Please enter a valid command.')
+
+while cont:
+  operate()
+  cont_boo = input('Would you like to translate another message? (y/n) ')
+  if cont_boo == 'n':
+    cont = False
