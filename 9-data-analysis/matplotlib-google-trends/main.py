047dd3453392100598a225ac891505f5cd59d0c5
Lim En Xi <limenxi07@gmail.com>
2022-06-16 11:30:12 +0800

    init

---
+++ 9-data-analysis/matplotlib-google-trends/main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df_tesla = pd.read_csv('TESLA Search Trend vs Price.csv')
+df_btc_search = pd.read_csv('Bitcoin Search Trend.csv')
+df_btc_price = pd.read_csv('Daily Bitcoin Price.csv')
+df_unemployment = pd.read_csv('UE Benefits Search vs UE Rate 2004-19.csv')
+
