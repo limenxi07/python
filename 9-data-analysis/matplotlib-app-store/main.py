1152b76be585b7ed42ac2b27cf2ef4dc0977c7cb
Lim En Xi <limenxi07@gmail.com>
2022-06-21 11:10:53 +0800

    plotly chart

--- 9-data-analysis/matplotlib-app-store/main.py
+++ 9-data-analysis/matplotlib-app-store/main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import plotly.express as px
 
 df = pd.read_csv('9-data-analysis/matplotlib-app-store/apps.csv')
 df.drop(['Last_Updated', 'Android_Ver'], axis=1, inplace=True)
@@ -17,3 +17,9 @@ for i in range(1, 6):
 df.sort_values('Size_MBs', ascending=False)
 print(f'Largest app: {df.iloc[0]["App"]} ({df.iloc[0]["Size_MBs"]} MB)')
 
+
+# DATA VISUALISATION - PLOTLY
+rt = df.Content_Rating.value_counts()
+f1 = px.pie(labels=rt.index, values=rt.values, title='Content Rating', names=rt.index, hole=0.6)
+f1.update_traces(textposition='inside', textinfo='percent', textfont_size=15)
+f1.show()
