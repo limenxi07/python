71df2aed7d8105827e680fd7cc252ed601248f22
Lim En Xi <limenxi07@gmail.com>
2022-06-27 20:20:37 +0800

    rectangle properties

--- 2-fcc-projects/polygon-area-calc.py
+++ 2-fcc-projects/polygon-area-calc.py
@@ -1,5 +1,23 @@
 class Rectangle:
-  pass
+
+  def __init__(self, width, height):
+    self.width = width
+    self.height = height
+  
+  def set_width(self, width):
+    self.width = width
+  
+  def set_height(self, height):
+    self.height = height
+
+  def get_area(self):
+    return self.width * self.height
+  
+  def get_perimeter(self):
+    return 2 * self.width + 2 * self.height
+  
+  def get_diagonal(self):
+    return (self.width ** 2 + self.height ** 2) ** .5
 
 
 class Square:
