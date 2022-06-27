089656735bdb55ae7459b648a84832d57051c132
Lim En Xi <limenxi07@gmail.com>
2022-06-27 18:51:18 +0800

    completed chart func

--- 2-fcc-projects/budget-app.py
+++ 2-fcc-projects/budget-app.py
@@ -70,22 +70,18 @@ def create_spend_chart(list):
 
   # label formatting
   chart.append('    ' + '-' * (len(categories) * 3 + 1))
-
-
+  length = max(len(i['name']) for i in categories)
+  chart += length * ['     ']
+  for i in range(length):
+    for category in categories:
+      if len(category['name']) > i:
+        chart[13+i] += (category['name'][i] + '  ')
+      else:
+        chart[13+i] += '   '
 
   return '\n'.join(chart) 
 
-food = Category("Food")
-entertainment = Category("Entertainment")
-business = Category("Business")
-food.deposit(900, "deposit")
-entertainment.deposit(900, "deposit")
-business.deposit(900, "deposit")
-food.withdraw(105.55)
-entertainment.withdraw(33.40)
-business.withdraw(10.99)
-print('EXPECTED', "Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  ")
-print(create_spend_chart([business, food, entertainment]))
+
 
 
 import unittest
@@ -187,5 +183,5 @@ class UnitTests(unittest.TestCase):
         expected = "Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  "
         self.assertEqual(actual, expected, 'Expected different chart representation. Check that all spacing is exact.')
 
-# if __name__ == "__main__":
-#     unittest.main()
+if __name__ == "__main__":
+    unittest.main()
